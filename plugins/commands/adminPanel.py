ddedd5a286b8a062f4f1da562a2a76337d9f1eb3
Animesh Das <64898084+karan0987@users.noreply.github.com>
2024-12-26 13:32:15 +0000

    Admin Access Bug fixed

--- plugins/commands/adminPanel.py
+++ plugins/commands/adminPanel.py
@@ -1,9 +1,11 @@
 from pyrogram import Client, filters  # type: ignore
 from functions import adminPanel
+from middleware.authAdmin import authAdmin
 
 
 @Client.on_message(filters.command(["admin", "panel", "adminPanel", "adminpanel"]))
 async def handleAdminCommand(_, msg):
+    if not authAdmin(_,msg):return
     text, keyboard = adminPanel(msg.from_user)
     await msg.reply(text, reply_markup=keyboard)
     pass
